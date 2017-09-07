8cab86c5e13af6447226e97a286757d377ebec39
andrii <andrii.prikhodko@smashdocs.net>
2017-09-07 15:17:32 +0300

    autotest booking

--- todo-spec.js
+++ todo-spec.js
@@ -1,17 +1,24 @@
-describe('angularjs homepage todo list', function() {
-  it('should add a todo', function() {
-    browser.get('https://angularjs.org');
+var until = protractor.ExpectedConditions;
 
-    element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-    element(by.css('[value="add"]')).click();
+describe('QA automation', function() {
+  it('Check booking', function() {
+    browser.ignoreSynchronization = true;
+    browser.get('https://www.booking.com');
 
-    var todoList = element.all(by.repeater('todo in todoList.todos'));
-    expect(todoList.count()).toEqual(3);
-    expect(todoList.get(2).getText()).toEqual('write first protractor test');
+    element(by.id('ss')).sendKeys('New York');
+    browser.wait(until.presenceOf(element(by.css('[data-list-item]'))), 5000, 'Element taking too long to appear in the DOM');
+    element(by.css('[data-list-item]')).click();
+    // browser.sleep(5000);
+    element(by.xpath('// div [data-placeholder="Check-in date"]')).click();
+    browser.sleep(5000);
 
-    // You wrote your first test, cross it off the list
-    todoList.get(2).element(by.css('input')).click();
-    var completedAmount = element.all(by.css('.done-true'));
-    expect(completedAmount.count()).toEqual(2);
+    // var todoList = element.all(by.repeater('todo in todoList.todos'));
+    // expect(todoList.count()).toEqual(3);
+    // expect(todoList.get(2).getText()).toEqual('write first protractor test');
+    //
+    // // You wrote your first test, cross it off the list
+    // todoList.get(2).element(by.css('input')).click();
+    // var completedAmount = element.all(by.css('.done-true'));
+    // expect(completedAmount.count()).toEqual(2);
   });
 });
