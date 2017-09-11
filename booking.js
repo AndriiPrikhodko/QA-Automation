59aa0c4b79269cadcb187e2efff7004651730add
andrii <andrii.prikhodko@smashdocs.net>
2017-09-11 11:58:31 +0300

    Assert results

--- booking.js
+++ booking.js
@@ -18,23 +18,11 @@ describe('QA automation', function() {
     element.all(by.css('td.c2-day-s-last-in-range')).first()
     .element(by.xpath('..'))
     .all(by.xpath("// span [contains(@class,'c2-day-inner') and text() = '" + (date.getDate() + time_span <= days_in_month ? date.getDate() + time_span : date.getDate() + time_span - days_in_month ) + "']"))
-    .get(date.getDate() + time_span < days_in_month ?  2*(date.getMonth()+1) - 1 : 2*(date.getMonth()+1) - 1 + 1 + date.getMonth()).click(); // values 8 and 9 taken from the website design
-    // element.all(by.css('button.sb-searchbox__button')).first().click();
-    browser.pause();
-    // .then(spans => spans.map(span => span.getText().then( text => console.log(text))));
-    //.get(21).click();
-    // element(by.xpath("// span [contains(@class,'c2-day-inner') and text() = '" + (date.getDate()+7) + "']")).click();
-    // then( spans => console.log(spans.length));
-    // element(by.xpath('..'))
-    // [contains(., '" + (date.getDate()+7) + "')]"
-    // var todoList = element.all(by.repeater('todo in todoList.todos'));
-    // expect(todoList.count()).toEqual(3);
-    // expect(todoList.get(2).getText()).toEqual('write first protractor test');
-    //
-    // // You wrote your first test, cross it off the list
-    // todoList.get(2).element(by.css('input')).click();
-    // var completedAmount = element.all(by.css('.done-true'));
-    // expect(completedAmount.count()).toEqual(2);
+    .get(date.getDate() + time_span < days_in_month ?  2*date.getMonth() : 2*date.getMonth()+1 ).click();
+    element.all(by.css('button.sb-searchbox__button')).first().click();
+    browser.wait(until.presenceOf(element(by.id('hotellist_inner'))), 5000, 'Element hotellist_inner not is not visible');
+    element.all(by.css('#hotellist_inner a.visited_link'))
+    .then(links => links.map(link => expect(link.getText()).toContain("New York")))//.then( text => console.log(text))))
   });
 });
 
